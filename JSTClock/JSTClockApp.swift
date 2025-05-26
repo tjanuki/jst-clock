af1de581b18167b35dabe75092b4b2c97f880b84
tjanuki <t.januki@gmail.com>
2025-05-26 17:29:39 -0400

    Initial Commit

---
+++ JSTClock/JSTClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSTClockApp.swift
+//  JSTClock
+//
+//  Created by Tetsuya Januki on 2025-05-26.
+//
+
+import SwiftUI
+
+@main
+struct JSTClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JSTClockTests/JSTClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  JSTClockTests.swift
+//  JSTClockTests
+//
+//  Created by Tetsuya Januki on 2025-05-26.
+//
+
+import Testing
+@testable import JSTClock
+
+struct JSTClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
